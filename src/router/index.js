3cec3ce057a37302f8922712ec4a4c0a13734826
Melvin Batista <mbatista@infrasystpr.com>
2022-10-24 17:37:46 -0400

    folder structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/Home.vue'
+import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Jobs from '../views/jobs/Jobs.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: HomeView
+    component: Home
   },
   {
     path: '/about',
     name: 'About',
     component: About
+  },
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs
   }
 ]
 
