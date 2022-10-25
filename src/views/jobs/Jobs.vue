365d41e60111042c14ce046f3f2ff40c9e1b43b3
Melvin Batista <mbatista@infrasystpr.com>
2022-10-25 00:31:50 -0400

    fetching data from components

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -11,12 +11,15 @@
 export default {
   data() {
     return {
-      jobs: [
-        { title: "Ninja US Designer", id: 1, details: "lorem" },
-        { title: "Ninja Web Developer", id: 2, details: "lorem" },
-        { title: "Ninja Vue Developer", id: 3, details: "lorem" },
-      ],
+      jobs: [],
     };
+  },
+  // prettier-ignore
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then(res => res.json())
+      .then(data => this.jobs = data)
+      .catch(err => console.log(err.message))
   },
 };
 </script>
